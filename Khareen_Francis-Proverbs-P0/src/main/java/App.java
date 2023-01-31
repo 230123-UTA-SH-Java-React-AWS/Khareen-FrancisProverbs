e9ab1a9a07f435cb2268f2ec5a445e41723aa9e3
khareenp <khareenp1@gmail.com>
2023-01-31 01:48:42 -0500

    added maven support

---
+++ Khareen_Francis-Proverbs-P0/src/main/java/App.java
@@ -0,0 +1,11 @@
+import com.revature.Employee;
+
+public class App {
+
+  public static void main(String[] args) throws Exception {
+    System.out.println("Hello, World!");
+
+    //create an employee object and have emp1 point to this object
+    Employee emp1 = new Employee();
+  }
+}
---
+++ Khareen_Francis-Proverbs-P0/src/main/java/com/revature/Employee.java
@@ -0,0 +1,4 @@
+//Package is a virtual folder that will store classes created.
+package com.revature;
+
+public class Employee {}
