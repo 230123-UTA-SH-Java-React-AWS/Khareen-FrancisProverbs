ade03e5103c0417be7c70ccba96e5ce6ca112030
khareenp <khareenp1@gmail.com>
2023-01-31 03:47:38 -0500

    uncomment closing curly brace

--- Khareen-FrancisProvers-P0/src/com/revature/model/Employee.java
+++ Khareen-FrancisProvers-P0/src/com/revature/model/Employee.java
@@ -9,62 +9,59 @@ import java.util.List;
 */
 
 public class Employee {
-    //Uniquely identifiable field that is numeric
-    private int employeeID;
-    
-    //Fields that will be useful to uniquely identify this object
-    private String name;
-    private String email;
-    private String password;
-    
 
-    //What data structure allows us to add multiple things
-    private List<Ability> abilities;
+  //Uniquely identifiable field that is numeric
+  private int employeeID;
 
-    public Employee() {
-        // this.abilities = new ArrayList<>();
-    }
+  //Fields that will be useful to uniquely identify this object
+  private String name;
+  private String email;
+  private String password;
 
-    
-    //right click, click source action, click generate getters and setters, select all fields, hit ok
-    public int getemployeeID() {
-        return employeeID;
-    }
+  //What data structure allows us to add multiple things
+  private List<Ability> abilities;
 
-    public void setemployeeID(int employeeID) {
-        this.employeeID = employeeID;
-    }
+  public Employee() {
+    // this.abilities = new ArrayList<>();
+  }
 
-    public String getName() {
-        return name;
-    }
+  //right click, click source action, click generate getters and setters, select all fields, hit ok
+  public int getemployeeID() {
+    return employeeID;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setemployeeID(int employeeID) {
+    this.employeeID = employeeID;
+  }
 
-    public String getEmail() {
-        return email;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public String getPassword() {
-        return password;
-    }
+  public String getEmail() {
+    return email;
+  }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+  public void setEmail(String email) {
+    this.email = email;
+  }
 
+  public String getPassword() {
+    return password;
+  }
 
-//     public List<Ability> getAbilities() {
-//         return abilities;
-//     }
+  public void setPassword(String password) {
+    this.password = password;
+  }
+  //     public List<Ability> getAbilities() {
+  //         return abilities;
+  //     }
 
-//     public void setAbilities(List<Ability> abilities) {
-//         this.abilities = abilities;
-//     }
-// }
+  //     public void setAbilities(List<Ability> abilities) {
+  //         this.abilities = abilities;
+  //     }
+}
